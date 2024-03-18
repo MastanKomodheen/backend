23e2f853831fe49725dd11cb417e329482de9fba
Mastan <komodheen.mastan@krazybee.com>
2024-03-18 14:21:42 +0530

    coverimagelocalpath added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -38,13 +38,13 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     // console.log(req.files)
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     //cover image is there or not
-    // let coverImageLocalPath;
-    // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-    //     coverImageLocalPath = req.files.coverImage[0].path;
-    // }
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is reqire")
